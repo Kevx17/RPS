ca1b3b765b8f5f07dae3adab807c92790cf6e781
Koller Kevin <kevinkekoller1@gmail.com>
2024-03-03 16:18:23 +0100

    its able to play 1 game of RPS

--- script.js
+++ script.js
@@ -12,18 +12,52 @@ function GetComputerChoice() {
         return("Scissors");
     }
 }
-
+// check checks wether the users input is correct (1, 2 or 3)
 let  check = false
 let computer = GetComputerChoice()
+let user
 
-while (!check) {
-    
-let user = prompt("Rock, Paper, Scissors! Enter 1 for Rock, 2 for Paper and 3 for Scissors: ");
+do {
+        
+user = prompt("Rock, Paper, Scissors! Enter 1 for Rock, 2 for Paper and 3 for Scissors: ");
 
-if (Number(user) == 1 && Number(user) == 2  && Number(user) == 3) {
+if (Number(user) == 1 || Number(user) == 2  || Number(user) == 3) {
    check = true;
+   if (user == 1){
+    user = "Rock";
 }
+else if (user == 2){
+    user = "Paper";
+}
+else {
+    user = "Scissors";
+}
+}
+
 else{
-  console.log{"please enter Enter 1 for Rock, 2 for Paper and 3 for Scissors!"};
+  console.log("please enter Enter 1 for Rock, 2 for Paper and 3 for Scissors!");
 }
+
+} while (!check);
+
+if (computer == user) {
+    console.log("its a tie! both of you chose " + user + "!");
+}
+else if (computer == "Scissors" && user == "Rock"){
+    console.log("You Win! Rock Beats Scissors!");
+}
+else if (computer == "Scissors" && user == "Paper"){
+    console.log("You Lose! Scissors Beats Paper!");
+}
+else if (computer == "Paper" && user == "Rock"){
+    console.log("You Lose! Paper Beats Rock!");
+}
+else if (computer == "Paper" && user == "Scissors"){
+    console.log("You Win! Scissors Beats Paper!");
+}
+else if (computer == "Rock" && user == "Paper"){
+    console.log("You Win! Paper Beats Rock!");
+}
+else if (computer == "Rock" && user == "Scissors"){
+    console.log("You Lose! Rock Beats Scissors!");
 }
