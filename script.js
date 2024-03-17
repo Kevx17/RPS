f444f05f1b8d93ab21c7d67591aa6975bd836af3
Koller Kevin <kevinkekoller1@gmail.com>
2024-03-17 10:28:08 +0100

    the ui has buttons but no messasges and the game is played till infinitie

--- script.js
+++ script.js
@@ -3,72 +3,52 @@ function GetComputerChoice() {
     let i = Math.floor(Math.random() * 3); 
     //get the choice of the computer based on the random number 
     if (i == 0){
-        return("Rock");
+        return(1);
     }
     else if (i == 1){
-        return("Paper");
+        return(2);
     }
     else {
-        return("Scissors");
+        return(3);
     }
 }
-// check checks wether the users input is correct (1, 2 or 3)
-let  check = false;
 let computer;
 let count1 = 0;
 let count2 = 0; 
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
 
 
 function playgame(user) {
 
 computer = GetComputerChoice();
-do {
-        
-//user = prompt("Rock, Paper, Scissors! Enter 1 for Rock, 2 for Paper and 3 for Scissors: ");
 
-if (Number(user) == 1 || Number(user) == 2  || Number(user) == 3) {
-   check = true;
-   if (user == 1){
-    user = "Rock";
-}
-else if (user == 2){
-    user = "Paper";
-}
-else {
-    user = "Scissors";
-}
-}
-//handle if the user inputs a nonacceptable input
-else{
-  console.log("please enter Enter 1 for Rock, 2 for Paper and 3 for Scissors!");
-}
-
-} while (!check);
 //after we got the users input we compere it to the computers choice and increment the counter variables based on the results
 if (computer == user) {
-    console.log("its a tie! both of you chose " + user + "!");
+    console.log("its a tie!");
 }
-else if (computer == "Scissors" && user == "Rock"){
+else if (computer == 3 && user == 1){
     console.log("You Win! Rock Beats Scissors!");
     count2++;
 }
-else if (computer == "Scissors" && user == "Paper"){
+else if (computer == 3 && user == 2){
     console.log("You Lose! Scissors Beats Paper!");
     count1++;
 }
-else if (computer == "Paper" && user == "Rock"){
+else if (computer == 2 && user == 1){
         console.log("You Lose! Paper Beats Rock!");
         count1++;
     }
-else if (computer == "Paper" && user == "Scissors"){
+else if (computer == 2 && user == 3){
         console.log("You Win! Scissors Beats Paper!");
         count2++;
     }
-else if (computer == "Rock" && user == "Paper"){
+else if (computer == 1 && user == 2){
         console.log("You Win! Paper Beats Rock!");
         count2++;
     }
-else if (computer == "Rock" && user == "Scissors"){
+else if (computer == 1 && user == 3){
         console.log("You Lose! Rock Beats Scissors!");
         count1++;
     }
@@ -78,7 +58,7 @@ else if (computer == "Rock" && user == "Scissors"){
     
 
 //plays a game of 5 rounds
-for (let i = 1; i <= 5; i++) {
+/*for (let i = 1; i <= 5; i++) {
         playgame(prompt("Rock, Paper, Scissors! Enter 1 for Rock, 2 for Paper and 3 for Scissors: "));
     }
 if (count2 > count1) {
@@ -89,7 +69,19 @@ else if (count2 == count1){
 }
 else{
     console.log("You Lose!");
-}
-    
-   
-   
+}**/
+
+
+
+
+btnRock.addEventListener("click", function() {
+    playgame(1);
+});
+
+btnPaper.addEventListener("click", function() {
+    playgame(2);
+});
+
+btnScissors.addEventListener("click", function() {
+    playgame(3);
+});
